6c51271ccb16b75f210b10163b3b05d22be5990a
Sumiya.Kogo <lionboy0043@gmail.com>
2019-03-09 19:24:33 +0900

    TableViewを作成する

--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MealViewController
 //  FoodTracker
 //
 //  Created by LT0083-PC on 2019/02/16.
@@ -8,12 +8,71 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class MealViewController: UIViewController, UITextFieldDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    //MARK: Properties
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var photoImageView: UIImageView!
+    @IBOutlet weak var ratingControl: RatingControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // Handle the text field’s user input through delegate callbacks.
+        nameTextField.delegate = self
     }
+    
+    //MARK: UITextFieldDelegate
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        // Hide the keyboard.
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        
+    }
+    
+    //MARK: UIImagePickerControllerDelegate
+    // ユーザーがイメージピッカーの[キャンセル]ボタンをタップしたときに呼び出されます。
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        // Dismiss the picker if the user canceled.
+        // イメージピッカーコントローラの解除をアニメーション化します。
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        // チュートリアルの記述はswift4.2で変更されたそうです。
+        // The info dictionary may contain multiple representations of the image. You want to use the original.
+        guard let selectedImage = info[.originalImage] as? UIImage else {
+            fatalError("Expected a dictionary containing an image, but was provided the following: \(info)")
+        }
+        
+        // Set photoImageView to display the selected image.
+        photoImageView.image = selectedImage
+        
+        // Dismiss the picker.
+        dismiss(animated: true, completion: nil)
+    }
+    
+    
+    //MARK: Actions
+    @IBAction func selectImageFromPhotoLibrary(_ sender: UITapGestureRecognizer) {
+        // Hide the keyboard.
+        //ユーザがテキストフィールドに入力している間に画像ビューをタップするとキーボードが正しく閉じられる
+        nameTextField.resignFirstResponder()
+        // UIImagePickerController is a view controller that lets a user pick media from their photo library.
+        let imagePickerController = UIImagePickerController()
+        // Only allow photos to be picked, not taken.
+        //イメージの取得する場所を設定
+        //photoLibraryオプションによりカメラロールを使用
+        //列挙型を推測してくれている
+        imagePickerController.sourceType = .photoLibrary
+        imagePickerController.delegate = self
+        // メソッドは暗黙的selfオブジェクトに対して実行されます
+        present(imagePickerController, animated: true, completion: nil)
+    }
+    
 
 
 }
